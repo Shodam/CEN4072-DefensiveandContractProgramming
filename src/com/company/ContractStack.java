2b966ba30326927bb17c4618b4574a93c81e30a1
Nova-Blue <85043707+Nova-Blue@users.noreply.github.com>
2021-09-06 13:41:24 -0400

    Added minimum functionality

--- src/com/company/ContractStack.java
+++ src/com/company/ContractStack.java
@@ -2,5 +2,25 @@ package com.company;
 
 public class ContractStack
 {
+    private final int CAPACITY = 5;
+    private int size;
+    private int[] stack;
 
+    public ContractStack() {
+        stack = new int[CAPACITY];
+        size = 0;
+    }
+
+    public void push(int x) {
+        stack[size] = x;
+        ++size;
+    }
+
+    public void pop() {
+        --size;
+    }
+
+    public int peek() {
+        return stack[size - 1];
+    }
 }
