60906a46ba90d17eab12544a3428676974cdfc70
thumperml <90116005+thumperml@users.noreply.github.com>
2021-09-07 17:02:43 -0400

    Update ContractTester.java

--- src/com/company/ContractTester.java
+++ src/com/company/ContractTester.java
@@ -1,12 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class ContractTester
 {
   
   public static void main (String[] args)
   {
+    ContractStack stack = new ContractStack();
+    Scanner scnr = new Scanner(System.in);
+    int userInput;
+    String exitCommand;
     
-    ContractStack <Integer> stack = new ContractStack <Integer>();
+    
+    //ContractStack <Integer> stack = new ContractStack <Integer>();
     
   }
   
