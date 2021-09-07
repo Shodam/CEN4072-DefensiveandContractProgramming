1767976154339a90940d78351c3db2e5f043a2fd
thumperml <90116005+thumperml@users.noreply.github.com>
2021-09-07 18:32:43 -0400

    Update ContractTester.java

--- src/com/company/ContractTester.java
+++ src/com/company/ContractTester.java
@@ -10,7 +10,33 @@ public class ContractTester
     ContractStack stack = new ContractStack();
     Scanner scnr = new Scanner(System.in);
     int userInput;
-    String exitCommand;
+    boolean exit = false;
+    
+    while (!exit)
+    {
+      System.out.println("Would you like to");
+      System.out.println("\t(1) Exit");
+      System.out.println("\t(2) Enter Value to Stack");
+      System.out.print(": ");
+      userInput = scnr.nextInt();
+      System.out.println();
+      
+      if (userInput == 1)
+      {
+        exit = true;
+      }
+      else if (userInput == 2)
+      {
+        System.out.print("Enter a value: ");
+        userInput = scnr.nextInt();
+        stack.stack_push(userInput);
+      }
+      else {
+        System.out.println("Invalid Choice");
+      }
+    
+    
+    
     
     
     //ContractStack <Integer> stack = new ContractStack <Integer>();
