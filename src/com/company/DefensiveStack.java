9bf793e8f2e2af01e99dd27671eb089093521a70
ArielGautier <77650271+ArielGautier@users.noreply.github.com>
2021-09-06 20:04:39 -0400

    Update DefensiveStack.java
    Added some preconditions and comments
    changed java.util.* -> java.util.stack

--- src/com/company/DefensiveStack.java
+++ src/com/company/DefensiveStack.java
@@ -1,25 +1,46 @@
 package com.company;
 
-import java.util.*;
+import java.util.Stack;
+import java.util.EmptyStackException;
 
 public class DefensiveStack
 {
+	private final int CAPACITY = 5;
+    private int size;
     private Stack<Integer> stack;
     
     public DefensiveStack() {
         stack = new Stack<Integer>();
+        size = 0;
     }
     
+    /**
+     * Precondition: Stack must not be at capacity
+     * Precondition: must push an Integer to the stack
+     * Postcondition: The integer x is added to the top of the stack
+     */
     public void stack_push(int x)
     {
-        stack.push(x);
+    	if(size < CAPACITY)
+    		throw new IndexOutOfBoundsException("Capacity of Stack is " + CAPACITY);
+        
+    	stack.push(x);
     }
 
+    /**
+     * Precondition: Stack must not be empty
+     */
     public void stack_pop()
     {
+    	if(size > 0)
+    		throw new EmptyStackException();
+    	
         stack.pop();
     }
 
+    /**
+     * @return the Integer at the top of the Stack
+     */
     public int stack_peek()
     {
         return stack.peek();
