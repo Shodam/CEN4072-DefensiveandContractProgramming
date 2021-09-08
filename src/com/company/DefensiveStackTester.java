75dca83fe7a10eaf1c724661b18eb354635066dc
ArielGautier <77650271+ArielGautier@users.noreply.github.com>
2021-09-08 16:59:06 -0400

    Update DefensiveStackTester.java

--- src/com/company/DefensiveStackTester.java
+++ src/com/company/DefensiveStackTester.java
@@ -1,4 +1,3 @@
-  
 package com.company;
 
 import java.util.Scanner;
@@ -19,8 +18,14 @@ public class DefensiveStackTester {
         
         while(userActive)
         {
-                System.out.println("Type exit to leave the program, type delete to remove the top of the stack, type ");
-                System.out.println("Enter a number to insert to the stack");
+                //System.out.println("Type exit to leave the program, type remove to delete the top of the stack");
+                System.out.println("Type: ");
+                System.out.println("Exit:\t To stop the program");
+                System.out.println("Remove:\t To delete the top of the stack");
+                System.out.println("Peek:\t To get int at the top of the stack");
+                System.out.println("Search:\t To search for a number in  the stack");
+                                   
+                System.out.println("\nEnter a number to insert to the stack");
                 userInput = input.nextLine();
                 Scanner intChecker = new Scanner(userInput.trim());
                 if (userInput.equalsIgnoreCase(exitCommand) || userInput.equalsIgnoreCase(removeCommand) || userInput.equalsIgnoreCase(peekCommand)) 
