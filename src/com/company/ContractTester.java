8a0f983a8619a53cca52b0708bbd5e547ab2efc9
thumperml <90116005+thumperml@users.noreply.github.com>
2021-09-08 00:30:09 -0400

    Update ContractTester.java

--- src/com/company/ContractTester.java
+++ src/com/company/ContractTester.java
@@ -17,6 +17,12 @@ public class ContractTester
       System.out.println("Would you like to");
       System.out.println("\t(1) Exit");
       System.out.println("\t(2) Enter Value to Stack");
+      System.out.println("\t(3) Pop top of Stack");
+      System.out.println("\t(4) Peek top of Stack");
+      System.out.println("\t(5) Search for element in stack");
+      System.out.println("\t(6) Print Stack");
+      
+      
       System.out.print(": ");
       userInput = scnr.nextInt();
       System.out.println();
@@ -25,18 +31,42 @@ public class ContractTester
       {
         exit = true;
       }
+      
       else if (userInput == 2)
       {
         System.out.print("Enter a value: ");
         userInput = scnr.nextInt();
         stack.stack_push(userInput);
       }
+      
+      else if (userInput == 3)
+      {
+        stack.stack_pop();
+      }
+      
+      else if (userInput == 4)
+      {
+        System.out.println(stack.stack_peek());
+      }
+      
+      else if (userInput == 5)
+      {
+        System.out.print("Enter a value: ");
+        userInput = scnr.nextInt();
+        stack.stack_search(userInput);
+      }
+      
+      else if (userInput == 6)
+      {
+        stack.stack_print();
+      }
+      
       else {
         System.out.println("Invalid Choice");
       }
       
     }
-    
+    stack.stack_print();
     
     
     
